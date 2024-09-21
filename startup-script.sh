78e88e29146a02281ed70f18407a46a1f2dd6f8d
williammtan <william@tan.id>
2024-09-21 15:11:42 +0700

    update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -1,4 +1,3 @@
-bin_dir=$(dirname $(which python))
 project_id=$(gcloud projects describe $(gcloud config get-value project) --format="value(projectNumber)")
 
 sudo apt update && sudo apt -y install python3 python3-pip python3-venv git
@@ -10,6 +9,8 @@ python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 
+bin_dir=$(dirname $(which python3))
+
 echo "REDIS_SECRET_VERSION=projects/$project_id/secrets/shopping-redis/versions/1" > .env
 
 cd shopping/
@@ -19,3 +20,5 @@ if ! pgrep -fl scrapyd ; then
 fi
 
 $bin_dir/scrapyd-deploy
+
+# To run: bash <(curl -s https://raw.githubusercontent.com/williammtan/shopping-scraper/main/startup-script.sh)
