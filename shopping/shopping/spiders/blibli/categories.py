361caa5296523f8299688b9df58aee227f50c819
williammtan <william@tan.id>
2024-09-21 11:46:07 +0700

    update blibli categories to remove playwright

--- shopping/shopping/spiders/blibli/categories.py
+++ shopping/shopping/spiders/blibli/categories.py
@@ -1,4 +1,3 @@
-from scrapy_playwright.page import PageMethod
 import scrapy
 
 URL = "https://www.blibli.com/categories"
@@ -6,24 +5,6 @@ URL = "https://www.blibli.com/categories"
 class BlibliCategories(scrapy.Spider):
     name = "blibli_categories"
     start_urls = ["file:///Users/williamtan/Projects/shopping-scraper/shopping/blibli_categories.html"]
-    # custom_settings = {
-    #     "DOWNLOAD_HANDLERS": {
-    #         "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-    #         "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-    #     },
-    #     "TWISTED_REACTOR": "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
-    #     "PLAYWRIGHT_LAUNCH_OPTIONS": {
-    #         "headless": False,
-    #     } 
-    # }
-
-    # def start_requests(self):
-    #     yield scrapy.Request(url=URL, callback=self.parse, meta=dict(
-	# 			playwright = True,
-	# 			# playwright_include_page = True, 
-	# 			playwright_page_methods =[PageMethod('wait_for_selector', '.categories')],
-    #             cookiejar = 0
-	# 		))
 
     def parse(self, response):
 
